0d18e2ae499270b206dceb9f40614512e3b53480
Roshan Kumar <roshankumarrk7488@gmail.com>
2025-03-17 18:34:52 +0530

    Updates

--- static/js/952.14612d7a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdigihealth=self.webpackChunkdigihealth||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.14612d7a.chunk.js.map
